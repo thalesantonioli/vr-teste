c2c380b5cbaaa24aaaa5ae18a7cad5cb5f9bfd16
Thales Antonioli <thales.antonioli165@gmail.com>
2022-10-06 22:13:17 -0300

    configuração inicial do projeto

---
+++ mini-autorizador/src/test/java/com/testevr/miniautorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testevr.miniautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
